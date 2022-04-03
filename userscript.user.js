c313221160e7fad2c27f65244f8603bfb2587880
Tyler Rehak <cubsfan518@gmail.com>
2022-04-03 15:27:43 -0400

    New dimensions

--- userscript.user.js
+++ userscript.user.js
@@ -6,7 +6,7 @@
 // @author       Catalyst, Fa113n5tar
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://tagpro.koalabeast.com/images/favicon.ico
-// @updateURL    https://github.com/catalyst518/tagpro_place_template/raw/main/userscript.user.js
+// @updateURL    https://github.com/catalyst518/tagpro_place_template/raw/main/userscgit ript.user.js
 // @downloadURL  https://github.com/catalyst518/tagpro_place_template/raw/main/userscript.user.js
 // @grant        none
 // ==/UserScript==
@@ -16,7 +16,7 @@ if (window.top !== window.self){
 		(function (){
 			const i = document.createElement("img");
 			i.src = "https://github.com/catalyst518/tagpro_place_template/blob/main/template.png?raw=true";
-			i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+			i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
 			console.log(i);
 			return i;
 		})())}, false);
